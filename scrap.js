003f993f4787ab33b71f0cf8262ccdfd050d70eb
Elliot Whitehouse <71378523+ElliotW65@users.noreply.github.com>
2022-03-22 16:32:01 +0000

    Updated repo

--- scrap.js
+++
@@ -1,8 +0,0 @@
-/*             const groups = data.reduce((groups, item) => {
-                const group = (groups[item.location] || []);
-                group.push(item);
-                groups[item.location] = group;
-                return groups;
-                }, {}); */
-            
-            //console.log(groups);
